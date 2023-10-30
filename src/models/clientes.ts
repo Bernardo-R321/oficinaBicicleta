d51ca2694fc099c8cfd80682732bc5f672808506
FranBronstrup <francinebronstrup@gmail.com>
2023-10-30 10:35:29 -0300

    clientes

---
+++ src/models/clientes.ts
@@ -0,0 +1,31 @@
+/*import { BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Cidades } from "./cidades";
+import internal from "stream";
+
+@Entity('clientes')
+export class Clientes extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    public id: number
+
+    @Column()
+    public nome: string;
+
+    @Column()
+    public cpf: string;
+
+    @Column()
+    public email: string;
+
+    @Column()
+    public telefone: string;
+
+    @Column()
+    public endereco: string;
+
+    @Column()
+    public id_cidade: number;
+
+    @Column()
+    public estado: string;
+
+}*/
