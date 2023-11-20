27105c9ee2e2ff131bcbad779dc1af82a4c67658
FranBronstrup <francinebronstrup@gmail.com>
2023-11-20 20:37:39 -0300

    envio e-mail

--- backend/src/controller/StatusController.ts
+++ backend/src/controller/StatusController.ts
@@ -1,6 +1,9 @@
 import { Status } from '../models/status';
-import { ILike} from 'typeorm';
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { ILike } from "typeorm";
+import * as nodemailer from "nodemailer";
+import * as puppeteer from "puppeteer";
 
 export class StatusController {
 
@@ -46,4 +49,44 @@ export class StatusController {
     
         return res.status(200).json(status);
     }
+
+    async sendEmail(req: Request, res: Response): Promise <Response> {
+        let body = req.body;
+    
+        let emailConfig = {
+          host: "smtp.office365.com",
+          port: 587,
+          secure: false,
+          tls: {
+            rejectUnauthorized: false,
+            ciphers: "SSLv3",
+          },
+          auth: {
+            user: process.env.USER,
+            pass: process.env.PASS,
+          },
+        };
+    
+        if (body.status == "concluida") {
+            let mailOptions = {
+            from: "francinebronstrup@hotmail.com",
+            to: body.email,
+            subject: "Oficina Bike",
+            html: `Estamos entrando em contato para avisar que o conserto da sua bike está concluido, ${body.nome}. Venha até a loja retirar a sua magrela ;)`,
+            };
+    
+            let transporter = nodemailer.createTransport(emailConfig);
+        
+            transporter.sendMail(mailOptions, async function (error, info) {
+            if (error) {
+                console.log("Erro ao enviar email:" + error);
+                return res.status(401).send("Erro ao enviar email" + error);
+            } else {
+                console.log("Email enviado: " + info.response);
+                return res.status(200).send("Email enviado: " + info.response);
+            }
+            });
+        
+            return res.status(401);
+        }};
 }
